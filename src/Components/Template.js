8d31318c84fede12be12e7e9597c6bbfb5fdd112
nakyong82 <nakyong07@naver.com>
2021-11-19 17:04:56 +0900

    Update

--- src/Components/Template.js
+++ src/Components/Template.js
@@ -5,7 +5,7 @@ const Template = ({ todos, children }) => {
 
   return (
     <div className="Template">
-      <div className="title">Todo App ({ todosLength })</div> {/* todos의 길이가 반환이 안돼넹 ..*/}
+      <div className="title">Todo App ({ todosLength })</div> 
       <div className="content">{ children }</div>
     </div>
   );
